a9f25002f67eb72728f6216ee0cd3071c493afca
NateDoesCodin <pahilangaalfred@gmail.com>
2025-05-23 23:42:06 +0800

    Add files via upload

---
+++ finalRob/src/main/java/com/mycompany/finalrob/FinalRob.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.finalrob;
+
+/**
+ *
+ * @author User
+ */
+public class FinalRob {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
